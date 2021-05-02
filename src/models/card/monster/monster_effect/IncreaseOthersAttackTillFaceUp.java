0d9a4b76fc021858ccd9b31e0f2b8da4355ea410
MSharify <mohamadsharify36@gmail.com>
2021-05-02 12:29:28 +0430

    card

--- src/models/card/monster/monster_effect/IncreaseOthersAttackTillFaceUp.java
+++ src/models/card/monster/monster_effect/IncreaseOthersAttackTillFaceUp.java
@@ -1,6 +1,6 @@
 package models.card.monster.monster_effect;
 
-import card.monster.monster_effect.MonsterEffect;
+import models.card.monster.monster_effect.MonsterEffect;
 
 public class IncreaseOthersAttackTillFaceUp implements MonsterEffect {
     public void run(){
