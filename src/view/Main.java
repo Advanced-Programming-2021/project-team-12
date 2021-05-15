0f8d294938460ef41f1fcac5e02f30967ba7ad57
Mirzaee11 <alimirzaee381@gmail.com>
2021-05-15 12:05:32 +0430

    maven

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,11 +1,11 @@
 package view;
+import java.util.Scanner;
 import controllers.SaveFile;
 import models.User;
-import java.util.Scanner;
 
 public class Main{
     public static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args){
+    public static void main(String[] args) {
         User user = new User("aa", "rr", "kk");
         new SaveFile();
         new RegistrationMenu();
