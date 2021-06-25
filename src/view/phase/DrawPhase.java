159a7ad754d1fcfe1b9c6095ce6ac2c25a7a5c1e
Mirzaee11 <alimirzaee381@gmail.com>
2021-06-26 00:51:49 +0430

    .

--- src/view/phase/DrawPhase.java
+++ src/view/phase/DrawPhase.java
@@ -1,4 +1,5 @@
 package view.phase;
+import com.sun.source.tree.WhileLoopTree;
 import controllers.Game;
 import controllers.PhaseControl;
 import view.AIPhase.AIDrawPhase;
@@ -6,19 +7,22 @@ import view.Main;
 
 public class DrawPhase {
     public void run(){
+        System.out.println(Game.whoseRivalPlayer().getName());
         if (Game.isAITurn())
             new AIDrawPhase();
-        System.out.println(PhaseControl.getInstance().printWhoseTurnIsIt());
-        System.out.println("phase: draw phase");
-        String input;
-        System.out.println(PhaseControl.getInstance().drawOneCard());
-        while (true) {
-            input = Main.scanner.nextLine().trim();
-            if (input.matches("^[ ]*next phase[ ]*$"))
-                break;
-            else
-                System.out.println("invalid command");
+        else {
+            System.out.println(PhaseControl.getInstance().printWhoseTurnIsIt());
+            System.out.println("phase: draw phase");
+            String input;
+            System.out.println(PhaseControl.getInstance().drawOneCard());
+            while (true) {
+                input = Main.scanner.nextLine().trim();
+                if (input.matches("^[ ]*next phase[ ]*$"))
+                    break;
+                else
+                    System.out.println("invalid command");
+            }
+            Game.playTurn("StandByPhase");
         }
-        Game.playTurn("StandByPhase");
     }
 }
