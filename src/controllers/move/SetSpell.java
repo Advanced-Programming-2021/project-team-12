3ab46fb83cfce773ba812d0ff9e174ce52fc1c70
MSharify <mohamadsharify36@gmail.com>
2021-06-25 19:11:25 +0430

    m

--- src/controllers/move/SetSpell.java
+++ src/controllers/move/SetSpell.java
@@ -4,10 +4,6 @@ import models.Board;
 import controllers.Game;
 
 public class SetSpell {
-    public static boolean doAnyOneHaveForest(){
-        return Board.getAnyoneHave("Forest");
-    }
-
     public static boolean doIHaveMessengerOfPeace() {
         return Game.whoseTurnPlayer().doIHaveSpellCard("Messenger of Peace");
     }
@@ -16,19 +12,8 @@ public class SetSpell {
         Game.whoseTurnPlayer().removeOneOfTrapOrSpell("Messenger of Peace");
     }
 
-    public static boolean doIHaveClosedForest() {
-        return Game.whoseTurnPlayer().doIHaveSpellCard("Closed Forest");
-    }
-
-    public static boolean doAnyOneHaveUmiiruka() {
-        return Board.getAnyoneHave("Umiiruka");
-    }
-
-    public static boolean doAnyOneHaveMessengerOfPeace(){
+    public static boolean doAnyOneHaveMessengerOfPeace() {
         return Board.getAnyoneHave("Messenger of Peace");
-    }
 
-    public static boolean doAnyOneHaveYami(){
-        return Board.getAnyoneHave("Yami");
     }
 }
