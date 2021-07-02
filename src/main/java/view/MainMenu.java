5b600182027285f8e2c37421ed8b60833b2f9d3b
Mirzaee11 <alimirzaee381@gmail.com>
2021-07-02 19:51:50 +0430

    .

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -1,13 +1,30 @@
 package view;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import models.User;
 
 public class MainMenu {
     public static User user;
+    public static Stage stage;
+    public TextField userName;
+    public TextField nickName;
+    public PasswordField password;
 
-    public MainMenu(User firstUser) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        MainMenu.stage = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
-    public void key(ActionEvent actionEvent) {
+    public void setUser(User _user) {
+        user = _user;
     }
+
 }
