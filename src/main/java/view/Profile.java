735c52f9aa9b2fe7a150caa65eec37624e3e3b39
MSharify <mohamadsharify36@gmail.com>
2021-07-02 20:56:08 +0430

    m

--- src/main/java/view/Profile.java
+++ src/main/java/view/Profile.java
@@ -8,15 +8,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Profile extends Application {
     public static Stage stage;
-    public static TextField nickName;
     public static PasswordField pass1;
     public static PasswordField pass2;
+    public TextField nickName;
+    public TextField oldPassword;
+    public TextField newPassword;
+    public Label text;
+
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -24,5 +30,30 @@ public class Profile extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/SignIn.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void back(MouseEvent mouseEvent) throws Exception {
+        new MainMenu().start(stage);
+    }
+
+    public void changeNickname(MouseEvent mouseEvent) {
+        text.setText("");
+        try {
+            new ProfileControl().changeNickName(nickName.getText());
+            nickName.clear();
+        }catch (Exception e){
+          text.setText(e.getMessage());
+        }
+    }
+
+    public void changePassword(MouseEvent mouseEvent) {
+        text.setText("");
+        try {
+            new ProfileControl().changePass(oldPassword.getText(), newPassword.getText());
+            oldPassword.clear();
+            newPassword.clear();
+        }catch (MyException e){
+            text.setText(e.getMessage());
+        }
     }
 }
