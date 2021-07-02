466b89e89e0855921552730dbf331836b9158f2a
Mirzaee11 <alimirzaee381@gmail.com>
2021-07-02 17:30:44 +0430

    .

--- src/main/java/view/SignIn.java
+++ src/main/java/view/SignIn.java
@@ -10,17 +10,14 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class SignIN extends Application {
-    public GridPane gameBoard;
-    public Button startGameButton;
+public class SignIn extends Application {
     public BorderPane borderPane;
     private static Stage stage;
 
     @Override
     public void start(Stage stage) throws Exception {
-        new LoadFile();
-        RegistrationMenu.stage = stage;
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/RegistrationMenu.fxml"));
+        SignIn.stage = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SignIn.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
     }
