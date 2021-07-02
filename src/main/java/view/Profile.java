7112ad6a3ebf1ce719973d1f833cce799164c6f4
Mirzaee11 <alimirzaee381@gmail.com>
2021-07-03 00:15:23 +0430

    .

--- src/main/java/view/Profile.java
+++ src/main/java/view/Profile.java
@@ -1,10 +1,13 @@
 package view;
 import java.util.Objects;
 
+import Exceptions.MyException;
+import controllers.ProfileControl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,6 +18,9 @@ public class Profile extends Application {
     public static Stage stage;
     public static TextField nickName;
     public ImageView avatar;
+    public TextField oldPassword;
+    public TextField newPassword;
+    public Label text;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -33,5 +39,30 @@ public class Profile extends Application {
     public void rightAvatar(MouseEvent mouseEvent) {
         MainMenu.user.increaseAvatar();
         avatar.setImage(new Image(Objects.requireNonNull(getClass().getResource("/Characters/Chara001.dds" + MainMenu.user.getAvatar() + ".png")).toExternalForm()));
+    }
+
+    public void back(MouseEvent mouseEvent) throws Exception {
+        (new MainMenu()).start(stage);
+    }
+
+    public void changeNickname(MouseEvent mouseEvent) {
+        this.text.setText("");
+        try {
+            (new ProfileControl()).changeNickName(this.nickName.getText());
+        } catch (Exception var3) {
+            this.text.setText(var3.getMessage());
+        }
+
+    }
+
+    public void changePassword(MouseEvent mouseEvent) {
+        this.text.setText("");
+
+        try {
+            (new ProfileControl()).changePass(this.oldPassword.getText(), this.newPassword.getText());
+        } catch (MyException var3) {
+            this.text.setText(var3.getMessage());
+        }
+
     }
 }
