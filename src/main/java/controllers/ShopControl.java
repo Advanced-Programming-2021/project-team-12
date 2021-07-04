dae456717ba3c9e3d11012f64e78589b5d24406c
MSharify <mohamadsharify36@gmail.com>
2021-07-04 13:48:23 +0430

    m

--- src/main/java/controllers/ShopControl.java
+++ src/main/java/controllers/ShopControl.java
@@ -6,20 +6,15 @@ import models.User;
 import view.MainMenu;
 
 public class ShopControl {
-    public void buyCard(String cardName, User user) throws MyException {
-        Card card = Card.getCardByName(cardName);
-        if (card == null)
-            throw new MyException("there is no card with this name");
-        if (card.getPrice() > user.getMoney())
-            throw new MyException("not enough money");
-        else {
-            user.decreaseMoney(card.getPrice());
-            user.addCardToAllCard(card);
-            SaveFile.saveUser(user);
-        }
+    public void buyCard(Card card, User user) {
+        user.decreaseMoney(card.getPrice());
+        user.addCardToAllCard(card);
+        SaveFile.saveUser(user);
     }
 
     public void cheatyIncreaseMoney(int money) {
         MainMenu.user.increaseMoney(money);
     }
+
+
 }
