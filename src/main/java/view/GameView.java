89701d37301f74c0d32a67322aa37cd4f653bd35
ard11103 <ard_danaei@yahoo.com>
2021-07-04 22:07:12 +0430

    rweg4y45y

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -20,14 +20,28 @@ public class GameView extends Application {
         stage = primaryStage;
         Pane pane =  FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/GameView.fxml")));
         createBackground("normal", pane);
+        createBackgroundCards();
         createPlayers();
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
     }
 
+    private void createBackgroundCards() {
+//        Image image = new Image(getClass().getResource("/PNG/Cards/Monsters/Unknown.jpg").toExternalForm());
+//        ImageView imageView = new ImageView();
+//        imageView.setImage(image);
+//        imageView.setImage(image);
+//        imageView.setFitWidth(600.0);
+//        imageView.setFitHeight(600.0);
+//        imageView.setPreserveRatio(false);
+//        imageView.setX(200);
+//        imageView.setY(0);
+//        pane.getChildren().add(imageView);
+    }
+
     private void createPlayers() {
-//        File pictureFile =  new File(Game..getAvatarAddress());
+//        File pictureFile =  new File(.getAvatarAddress());
 //        String string = pictureFile.toURI().toString();
 //        Image image = new Image(getClass().getResource(string).toExternalForm());
 //        ImageView imageView = new ImageView();
@@ -41,15 +55,15 @@ public class GameView extends Application {
     }
 
     private void createBackground(String type, Pane pane) {
-        Image image = new Image(getClass().getResource("/PNG/Field/FieldPNG/fie_" + type + ".png").toExternalForm());
-        ImageView imageView = new ImageView();
-        imageView.setImage(image);
-        imageView.setImage(image);
-        imageView.setFitWidth(600.0);
-        imageView.setFitHeight(600.0);
-        imageView.setPreserveRatio(false);
-        imageView.setX(200);
-        imageView.setY(0);
-        pane.getChildren().add(imageView);
+//        Image image = new Image(getClass().getResource("/PNG/Field/FieldPNG/fie_" + type + ".png").toExternalForm());
+//        ImageView imageView = new ImageView();
+//        imageView.setImage(image);
+//        imageView.setImage(image);
+//        imageView.setFitWidth(600.0);
+//        imageView.setFitHeight(600.0);
+//        imageView.setPreserveRatio(false);
+//        imageView.setX(200);
+//        imageView.setY(0);
+//        pane.getChildren().add(imageView);
     }
 }
